474757a75478371fad69901df3e85a625fd2116f
Jose Cardona <105395738+joseCardona12@users.noreply.github.com>
2022-05-13 17:45:34 -0500

    Página Fundación con Vida FCV
    Trabajo solicitado por la docente de Herramientas Digitales. Procurando suministrar a la sociedad una posible variación en el diseño que la misma Fundación utiliza

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+//noticias//
+
+function mostrarNoticia(){
+    document.getElementById('noticias').style.display = 'block';
+}
+
+function ocultarNoticia(){
+    document.getElementById('noticias').style.display = 'none';
+}
